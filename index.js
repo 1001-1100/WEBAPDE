20fa483163abc39a12c73ef603cd5b4d46c4e4f4
1001-1110 <joshuacapulemanzano@gmail.com>
2018-07-09 13:57:49 +0800

    Login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cool = require('cool-ascii-faces')
 const path = require('path')
+const request = require('request')
 const PORT = process.env.PORT || 5000
 
 express()
@@ -9,6 +10,7 @@ express()
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.sendFile(path.join(__dirname,"index.html")))
 	.get('/signin', (req, res) => res.sendFile(path.join(__dirname,"signin.html")))
+	.get('/signedin', (req, res) => res.sendFile(path.join(__dirname,"signedin.html")))
     .get('/cool', (req, res) => res.send(cool()))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
